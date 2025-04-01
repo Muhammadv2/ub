32876bfb55f61652e5953a181502ccafd0f3faf4
Muhammadv2 <ipcloudflaretamiz@gmail.com>
2025-04-02 00:10:48 +0330

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -1,14 +1,57 @@
 #!/bin/bash
 
-# Option 1: Update and upgrade packages
-sudo apt update
-sudo apt upgrade -y
+display_main_menu() {
+    clear
+    echo "========================================"
+    echo "          System Update Menu          "
+    echo "========================================"
+    echo "Please select an option:"
+    echo "1) Update and Upgrade Packages"
+    echo "2) Install Update Manager Core"
+    echo "3) Configure Release Upgrades"
+    echo "4) Perform Release Upgrade"
+    echo "5) Exit Script"
+    echo "========================================"
+}
 
-# Option 2: Install update-manager-core
-sudo apt install update-manager-core -y
+while true; do
+    display_main_menu
+    read -p "Enter your choice (1-5): " choice
 
-# Option 3: Modify release-upgrades config
-sudo sed -i 's/^Prompt=lts$/Prompt=normal/' /etc/update-manager/release-upgrades
-
-# Option 4: Perform release upgrade
-sudo do-release-upgrade
+    case $choice in
+        1)
+            echo "Updating package lists and upgrading..."
+            sudo apt update
+            sudo apt upgrade -y
+            echo "Done! Press Enter to continue..."
+            read
+            ;;
+        2)
+            echo "Installing update-manager-core..."
+            sudo apt install update-manager-core -y
+            echo "Done! Press Enter to continue..."
+            read
+            ;;
+        3)
+            echo "Configuring release upgrades to 'normal'..."
+            sudo sed -i 's/^Prompt=lts$/Prompt=normal/' /etc/update-manager/release-upgrades
+            echo "Done! Press Enter to continue..."
+            read
+            ;;
+        4)
+            echo "Starting release upgrade..."
+            sudo do-release-upgrade
+            echo "Done! Press Enter to continue..."
+            read
+            ;;
+        5)
+            echo "Exiting script..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option! Please select 1-5."
+            echo "Press Enter to continue..."
+            read
+            ;;
+    esac
+done
